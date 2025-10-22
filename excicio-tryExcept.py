31fd1f9a3eb29390c464b8e6398a2dcf3083841d
Kauã Lira <programakaua2025gmail.com>
2025-10-22 17:06:03 -0300

    Todos feitos

---
+++ excicio-tryExcept.py
@@ -0,0 +1,39 @@
+#1
+# num1 = int(input("Digite um número: "))
+# num2 = int(input("Digite outro número: "))
+# try:
+#     print(num1 / num2)
+# except ZeroDivisionError:
+#     print("Impossível divisao por 0")
+#--------------------------------------------
+#2
+
+# try:
+#     numero = int(input("Digite um número: "))
+#     print(numero)
+# except ValueError:
+#     print("DIGITE UM NÚMERO!!!!!!!!!!!")
+
+#-------------------------------------------
+#3
+
+# try:
+#     num1 = int(input("Digite um número: "))
+#     num2 = int(input("Digite outro número: "))
+#     print(num1 / num2)
+# except ValueError:
+#     print("Digite número válido")
+# except ZeroDivisionError:
+#     print("Impossível divisão por 0")
+#--------------------------------------------
+#4
+# def dividir(a, b):
+#     return a / b
+
+# num1 = int(input("Digite um número: "))
+# num2 = int(input("Digite outro número: "))
+
+# try:
+#     print(dividir(num1, num2))
+# except ZeroDivisionError:
+#     print("Impossible division by 0")
---
+++ exercicio-funcao.py
@@ -0,0 +1,49 @@
+#1
+# def login(eemail, seenha):
+#     if email == 'admin' and senha == 'admin':
+#         return True
+
+#---------------------------------------
+#2
+# def preco(produto):
+#     return produto - (produto * 0.10)
+
+#--------------------------------------------
+#3
+# import funcoes_bolsa 
+# value = int(input("Digite o valor do bolsa: "))
+
+# print(funcoes_bolsa.bolsa(value))
+#-------------------------------------------
+# 4
+# def idade(valor): 
+#     if valor >= 18:
+#         return "Maior de idade"
+#     else: 
+#         return "Menor de idade"
+    
+# age = int(input("Digite sua idade"))
+# print(idade(age))
+#-----------------------------------------
+#5
+# def imc(peso, altura):
+#     return peso / (altura ** 2)
+
+# def classificar(valor):
+#     if valor < 18.5:
+#         return "Abaixo do peso"
+#     elif valor >= 18.5 and valor <= 24.9:
+#         return "Peso normal"
+#     else: 
+#         return "Acima do peso"
+
+# peso = float(input("Digite peso: "))
+# altura = float(input("Digite altura: "))
+
+# coleta = imc(peso, altura)
+
+# print(classificar(coleta))
+#----------------------------------------
+#6
+# def preco(produto):
+#      return produto + (produto * 0.15)
